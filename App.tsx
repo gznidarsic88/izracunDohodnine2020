be12031fcfb958fab8a11bacc87a2127870bcaa3
gznidarsic88 <gznidarsic88@gmail.com>
2021-02-07 09:29:55 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar, SafeAreaViewBase } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
+import { ActivityIndicator, Colors } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+
+export default function Aspp(){
+  const [text, setText] = React.useState('');
+  return(
+    <SafeAreaView style={styles.container2}>
+      <PaperProvider>
+
+      <Card>
+    <Card.Title title="Plače, nadomestilo plače"  />
+    <Card.Content>
+    <TextInput style={}
+      label="Dohodek54565sss64"
+      value={text}
+      onChangeText={text => setText(text)}
+    />
+    </Card.Content>
+  </Card>
+
+        <Text>dsssaddddddddddasdsddddddddasdsad</Text>
+
+        
+   
+      </PaperProvider>
+    
+    </SafeAreaView>
   );
 }
 
+
+
+ 
+
+
 const styles = StyleSheet.create({
-  container: {
+  container2: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    paddingTop:StatusBar.currentHeight
+    
+    //alignItems: 'center',
+
   },
+
 });
---
+++ batch.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Pritisni OK za zagon projekta \izracundohodnine2020
+
+cd C:\Users\Gregor\documents\programiranje\react native\izracundohodnine2020
+expo start
+PAUSE
